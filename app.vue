2ebe03106e3e45320673c2c66e23ebd8488881a0
freuwoert <33777960+freuwoert@users.noreply.github.com>
2024-05-20 21:22:31 +0200

    Add legal stuff Mixed and mashed with the design (more grunge)

--- app.vue
+++ app.vue
@@ -1,28 +1,25 @@
 <template>
-    <HeSection is="header" height="4rem" background="var(--bg-zinc-900)" shadow="l" sticky divider-position="absolute-outer">
+    <HeSection is="header" class="border-b border-b-zinc-800" shadow="l" sticky divider-position="absolute-outer">
         <HeLimiter>
             <HeFlex class="h-16" horizontal gap="1rem">
-                <HeLogo href="/" src="/images/chillen_auf_der_tanke_logo.png" alt="Logo Ipsum"/>
+                <HeLogo href="/" src="/images/logo.png" alt="Logo Ipsum"/>
                 <HeSpacer />
                 <HeMenu :menu="menu" mobile-submenu-show-toggle />
                 <IodButton is="a" size="s" corner="pill" label="Sei dabei" background="var(--bg-lime-400)" target="blank" href="https://calendar.app.google/XmvWRP3xdMFNfaKJ6"/>
             </HeFlex>
         </HeLimiter>
-        <template #below>
-            <HeShapeDivider shape="triangle" height=".5rem" background="var(--bg-zinc-900)"  flip-x/>
-        </template>
     </HeSection>
 
     <main>
         <NuxtPage />
     </main>
 
-    <HeSection class="border-t border-t-zinc-800" is="footer" background="var(--bg-zinc-900)">
+    <HeSection class="border-t border-t-zinc-800" is="footer">
         <HeLimiter>
             <HeFlex class="min-h-16" padding="1rem 0" horizontal gap="1rem 2rem" breakpoint="768">
                 <small>© {{ new Date().getFullYear() }} Alyx Freuwört</small>
-                <HeSpacer />
-                <NuxtLink to="/">Impressum und Datenschutz</NuxtLink>
+                <HeSpacer class="hidden md:flex"/>
+                <small><NuxtLink to="/legal">Impressum und Datenschutz</NuxtLink></small>
             </HeFlex>
         </HeLimiter>
     </HeSection>
@@ -37,3 +34,12 @@
         { id: 'faq', label: 'FAQ', href: '/#faq' },
     ])
 </script>
+
+<style lang="sass" scoped>
+    header
+        background-color: var(--bg-zinc-900)
+        background-image: url('/images/grunge.jpg') !important
+        background-size: cover
+        background-position: top center
+        background-blend-mode: screen
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,33 @@
 <template>
-    <HeSection id="home">
-        <HeLimiter size="text">
+    <HeSection id="home" class="border-b border-b-zinc-800">
+        <HeLimiter>
             <HeFlex class="hero" align-x="center">
-                <h1 class="m-0 text-center text-lime-400 text-5xl leading-snug font-semibold">Chilln' auf der Tanke</h1>
-                <h1 class="m-0 text-center text-zinc-200 text-3xl leading-normal font-normal">» Catered Rave im Lost Place «</h1>
-                <HeFlex horizontal gap="1rem" padding="2rem 0 0">
-                    <IodButton is="a" size="l" corner="pill" label="Sei jetzt dabei" shadow="l" background="var(--bg-lime-400)" target="_blank" href="https://calendar.app.google/XmvWRP3xdMFNfaKJ6"/>
-                    <IodButton is="a" size="l" corner="pill" label="Mehr erfahren" border shadow="l" variant="contained" background="var(--bg-lime-400)" href="/#about"/>
+                <h1 class="m-0 text-center text-lime-400 text-4xl leading-snug md:text-6xl md:leading-snug font-semibold">Chilln' auf der Tanke</h1>
+                <h1 class="m-0 mb-12 text-center text-lime-200 text-2xl leading-normal md:text-4xl md:leading-normal font-normal">Catered Rave im Lost Place</h1>
+                <HeFlex horizontal gap="1rem" breakpoint="768" class="min-w-72">
+                    <IodButton is="a" size="l" corner="pill" border variant="contained" label="Sei jetzt dabei" background="var(--bg-lime-400)" target="_blank" href="https://calendar.app.google/XmvWRP3xdMFNfaKJ6"/>
+                    <IodButton is="a" size="l" corner="pill" border variant="contained" label="Mehr erfahren" background="var(--bg-lime-200)" href="/#about"/>
                 </HeFlex>
             </HeFlex>
         </HeLimiter>
         <template #background>
             <img class="h-full w-full absolute inset-0 object-cover" src="/images/hero.jpg">
-            <div class="h-full w-full absolute inset-0 bg-zinc-900 opacity-80"></div>
+            <div class="h-full w-full absolute inset-0 bg-zinc-900 opacity-70"></div>
         </template>
     </HeSection>
 
-    <HeSection divider-position="absolute-outer" id="about">
+
+
+    <HeSection divider-position="absolute-outer" id="about" class="border-b border-b-zinc-800">
         <HeLimiter>
             <HeFlex padding="6rem 0" gap="4rem">
-                <HeFlex horizontal gap="2rem 4rem" align-y="flex-start" breakpoint="768">
+                <HeFlex horizontal gap="2rem 4rem" breakpoint="768">
                     <div class="flex-1">
-                        <img src="/images/about.png" class="flex-1 rounded-xl aspect-video"/>
+                        <img src="/images/about.jpg" class="flex-1 rounded-xl aspect-video"/>
                     </div>
                     <div class="flex-1">
-                        <h2 class="m-0 text-lime-400 text-4xl leading-snug font-semibold">Über CHADT</h2>
-                        <h3 class="m-0 mb-6 text-zinc-200 text-2xl leading-snug font-normal italic">Was is Chilln' auf der Tanke überhaupt?</h3>
+                        <h2 class="m-0 text-lime-400 text-5xl leading-snug font-semibold">Über CHADT</h2>
+                        <h3 class="m-0 mb-6 text-lime-200 text-2xl leading-snug font-normal italic">Was is Chilln' auf der Tanke überhaupt?</h3>
                         <p class="mb-4">
                             Was als kleine Geburtstagsfeier in einem Lost Place bei Salzgitter angefangen hat, hat sich schnell zu einem Outdoor Rave samt Catering weiterentwickelt.
                             Mit Chilln' auf der Tanke möchte ich die entspannten Summer Vibes eines Gartenfests mit dem Kick von Lost Places kombinieren.
@@ -37,59 +39,62 @@
                 </HeFlex>
             </HeFlex>
         </HeLimiter>
-
-        <template #above>
-            <HeShapeDivider shape="arrow" height="2rem" background="var(--bg-zinc-900)" inverted flip-x/>
-        </template>
     </HeSection>
 
 
 
-    <HeSection divider-position="absolute-outer" background="var(--bg-zinc-800)" id="when-and-where">
+    <HeSection divider-position="absolute-outer" id="when-and-where" class="border-b border-b-zinc-800" background="#00000050">
         <HeLimiter>
             <HeFlex padding="6rem 0" gap="4rem">
-                <HeFlex horizontal gap="2rem 4rem" align-y="flex-start" breakpoint="768" reversed>
+                <HeFlex horizontal gap="2rem 4rem" breakpoint="768" reversed>
                     <iframe
-                        class="flex-1 aspect-video rounded-xl"
+                        class="flex-1 aspect-video rounded-xl grayscale"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d363.94878376349305!2d10.414229456996376!3d52.14622035827743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a55a31f9578ebf%3A0xc4100315ba66210e!2sAm%20Gummibahnhof%209%2C%2038239%20Salzgitter!5e0!3m2!1sen!2sde!4v1716217824966!5m2!1sen!2sde"
                         allowfullscreen
                         loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade">
                     </iframe>
                     <div class="flex-1">
-                        <h2 class="m-0 text-lime-400 text-4xl leading-snug font-semibold">Wann und Wo</h2>
-                        <h3 class="m-0 mb-6 text-zinc-200 text-2xl leading-snug font-normal italic">Wann findet's statt und wo muss ich sein?</h3>
-                        <p class="mb-4">
-                            Angepeilt ist der <b>Samstag, 06. Juli 2024</b> ab ca. <b>15:00 Uhr</b>.<br>
-                            Die Location ist der <a href="https://maps.app.goo.gl/FxXNxfyXZyxGEMTt5" target="_blank"><b>Gummibahnhof 9 in Salzgitter</b></a>¹.
-                        </p>
-                        <p class="mb-6">
+                        <h2 class="m-0 text-lime-400 text-5xl leading-snug font-semibold">Wann und Wo</h2>
+                        <h3 class="m-0 mb-6 text-lime-200 text-2xl leading-snug font-normal italic">Wann findet's statt und wo muss ich sein?</h3>
+                        <HeDivider class="my-4"/>
+                        <HeFlex horizontal gap="1rem">
+                            <IodIcon class="text-lime-200" icon="today"/>
+                            <p>
+                                Datum: <b>Samstag, 06. Juli 2024</b> ab ca. <b>15:00 Uhr</b>
+                            </p>
+                        </HeFlex>
+                        <HeDivider class="my-4"/>
+                        <HeFlex horizontal gap="1rem">
+                            <IodIcon class="text-lime-200" icon="location_on"/>
+                            <p>
+                                Location: <a href="https://maps.app.goo.gl/FxXNxfyXZyxGEMTt5" target="_blank"><b>Gummibahnhof 9 in Salzgitter</b></a>
+                            </p>
+                        </HeFlex>
+                        <HeDivider class="my-4"/>
+                        <p class="mb-8">
                             Da die Location mit ÖPNV nicht all zu gut zu erreichen ist, empfehlen wir Fahrgemeinschaften. Parkplätze sind ausreichend vorhanden.
                             Fragt mich auch gerne persönlich, wenn ihr eine Mitfahrgelegenheit sucht.
                         </p>
-                        <IodButton is="a" label="Sei jetzt dabei" href="https://calendar.app.google/XmvWRP3xdMFNfaKJ6" target="_blank" corner="pill" background="var(--bg-lime-400)"/>
+                        <IodButton is="a" size="l" label="Sei jetzt dabei" href="https://calendar.app.google/XmvWRP3xdMFNfaKJ6" target="_blank" corner="pill" background="var(--bg-lime-400)"/>
                     </div>
                 </HeFlex>
             </HeFlex>
         </HeLimiter>
-
-        <template #above>
-            <HeShapeDivider shape="arrow" height="2rem" background="var(--bg-zinc-800)" inverted/>
-        </template>
     </HeSection>
 
 
 
-    <HeSection divider-position="absolute-outer" id="fee">
+    <HeSection divider-position="absolute-outer" id="fee" class="border-b border-b-zinc-800">
         <HeLimiter>
             <HeFlex padding="6rem 0" gap="4rem">
-                <HeFlex horizontal gap="2rem 4rem" align-y="flex-start" breakpoint="768">
+                <HeFlex horizontal gap="2rem 4rem" breakpoint="768">
                     <div class="flex-1">
-                        <img src="/images/about.png" class="flex-1 rounded-xl aspect-video"/>
+                        <img src="/images/fee.jpg" class="flex-1 rounded-xl aspect-video"/>
                     </div>
                     <div class="flex-1">
-                        <h2 class="m-0 text-lime-400 text-4xl leading-snug font-semibold">Eintritt</h2>
-                        <h3 class="m-0 mb-6 text-zinc-200 text-2xl leading-snug font-normal italic">Das kostet Geld!?</h3>
+                        <h2 class="m-0 text-lime-400 text-5xl leading-snug font-semibold">Eintritt</h2>
+                        <h3 class="m-0 mb-6 text-lime-200 text-2xl leading-snug font-normal italic">Das kostet Geld!?</h3>
                         <p class="mb-4">
                             Natürlich verlange ich <b>keinen Eintritt</b> für meine Geburtstagsfeier.
                             Da ich euch alle mit Catering und Getränken versorgen möchte, ist ein kleines, geldliches <b>Geburtstagsgeschenk (15-20€)</b> aber immer gerne gesehen.
@@ -98,22 +103,18 @@
                 </HeFlex>
             </HeFlex>
         </HeLimiter>
-
-        <template #above>
-            <HeShapeDivider shape="arrow" height="2rem" background="var(--bg-zinc-900)" inverted flip-x/>
-        </template>
     </HeSection>
 
 
 
-    <HeSection divider-position="absolute-outer" background="var(--bg-zinc-800)" id="faq">
+    <HeSection divider-position="absolute-outer" id="faq" background="#00000050">
         <HeLimiter>
             <HeFlex padding="6rem 0">
-                <h2 class="m-0 text-lime-400 text-4xl leading-snug font-semibold">FAQ</h2>
-                <h3 class="m-0 mb-6 text-zinc-200 text-2xl leading-snug font-normal italic">Was muss ich sonst noch wissen?</h3>
+                <h2 class="m-0 text-lime-400 text-5xl leading-snug font-semibold">FAQ</h2>
+                <h3 class="m-0 mb-6 text-lime-200 text-2xl leading-snug font-normal italic">Was muss ich sonst noch wissen?</h3>
                 <IodAccordion title="Ist das legal?" class="rounded-xl mb-2">
                     <p class="px-4 pb-4">
-                        Ja, Chilln' auf der Tanke ist eine private Geburtstagsfeier. Die Location ist ein öffentlicher Lost Place ohne Zugangsschutz und somit legal zu betreten.
+                        Ja, CHADT ist eine private Geburtstagsfeier. Die Location ist ein öffentlicher Lost Place ohne Zugangsschutz und legal zu betreten.
                         Das Tankstellengebäude ist jedoch nicht begehbar und wird auch nicht betreten.
                     </p>
                 </IodAccordion>
@@ -155,12 +156,15 @@
                 </IodAccordion>
             </HeFlex>
         </HeLimiter>
-
-        <template #above>
-            <HeShapeDivider shape="arrow" height="2rem" background="var(--bg-zinc-800)" inverted/>
-        </template>
     </HeSection>
 </template>
+
+<script lang="ts" setup>
+    useSeoMeta({
+        title: 'Chilln auf der Tanke',
+        description: 'Catered Rave im Lost Place',
+    })
+</script>
 
 <style lang="sass" scoped>
     .hero
